503d5bd4aba4b4d00f2babde807ebdbca6cae3c0
magoador <magomedov951.mail.ru>
2023-03-20 17:08:45 +0300

    Сделал model, route и controller для заказа

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(require('./routes/user.route'))
 
 app.use(morgan("dev"));
-app.use(require('./routes/cart.route'))
+app.use(require("./routes/cart.route"));
+app.use(require("./routes/user.route"));
+app.use(require("./routes/order.route"));
 app.use(express.static(__dirname + "/assets"));
 
 const { PORT, MONGO_SERVER } = process.env;
