a86376edacbfe62037fdd4cb48ed3eaf115fdfb2
magoador <magomedov951.mail.ru>
2023-03-20 14:40:13 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/assets"));
+
+const { PORT, MONGO_SERVER } = process.env;
+
+const connectAndStartServer = async () => {
+  try {
+    await mongoose.connect(MONGO_SERVER);
+    app.listen(PORT, () => {
+      console.log(`Server started: http://localhost:${PORT}/`);
+    });
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+connectAndStartServer();
