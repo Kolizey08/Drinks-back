1be8f6182b755089d8c0281228103c0ec265b72a
magoador <magomedov951.mail.ru>
2023-03-20 16:35:51 +0300

    Создал model, controller и route для корзины

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(morgan("dev"));
+app.use(require('./routes/cart.route'))
 app.use(express.static(__dirname + "/assets"));
 
 const { PORT, MONGO_SERVER } = process.env;
